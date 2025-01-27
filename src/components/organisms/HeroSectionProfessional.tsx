d15d3e5d1bb0622639912ce9757ed715c5a9a8cb
Enio Batalha <eniobatalha@gmail.com>
2025-01-26 22:33:04 -0300

    add hero section professional

---
+++ src/components/organisms/HeroSectionProfessional.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import React from "react";
+
+const HeroSectionProfessional: React.FC = () => {
+    return (
+        <div className="flex items-center justify-evenly bg-white shadow-md pt-10">
+            {/* Text Content */}
+            <div className="flex flex-col max-w-max">
+                <h1 className="text-6xl text-gray-900">
+                    Conecte-se a <span className="text-indigo-900 font-bold">milhares</span> de clientes <br/>
+                    e <span className="font-bold">cresça</span> sua carreira
+                </h1>
+
+                <span className="text-lg text-gray-600">
+          Ofereça seus serviços e encontre oportunidades de trabalho na sua região
+        </span>
+            </div>
+
+            <div className="hidden lg:block">
+                <img
+                    src="img/professional-hero.png"
+                    alt="Profissional"
+                    className="w-64 h-64 object-cover"
+                />
+            </div>
+        </div>
+    );
+};
+
+export default HeroSectionProfessional;
